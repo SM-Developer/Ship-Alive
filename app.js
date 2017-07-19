d43bfba11904e7430deafa04199de1176fc0f7df
clucle <wjdenwls123@gmail.com>
2017-07-19 22:04:31 +0900

    feat: get player input

--- app.js
+++ app.js
@@ -16,22 +16,35 @@ var SOCKET_LIST = {};
 
 var DEBUG = true;
 
+const Player = require('./server/player.js');
+var PLAYER_LIST = {};
+
 var io = require('socket.io')(serv, {});
 io.sockets.on('connection', function(socket) {
   socket.id = Math.random();
   SOCKET_LIST[socket.id] = socket;
   PLAYER_LIST[socket.id] = new Player(socket.id);
 
+  socket.on('keyPress', function (data) {
+    if (data.inputId === 'right') {
+      PLAYER_LIST[socket.id].pressRight = data.isPress;
+    }
+    else if (data.inputId === 'left') {
+      PLAYER_LIST[socket.id].pressLeft = data.isPress;
+    }
+    else if (data.inputId === 'up') {
+      PLAYER_LIST[socket.id].pressUp = data.isPress;
+    }
+    else if (data.inputId === 'down') {
+      PLAYER_LIST[socket.id].pressDown = data.isPress;
+    }
+  });
+
   socket.on('disconnect', function() {
     delete SOCKET_LIST[socket.id];
     delete PLAYER_LIST[socket.id];
   });
 });
-
-var input = require('./server/input.js')(io);
-
-const Player = require('./server/player.js');
-var PLAYER_LIST = {};
 
 
 setInterval(function() {
--- server/player.js
+++ server/player.js
@@ -6,11 +6,28 @@ class Player extends Ship{
 	constructor(id) {
 		super(0, 0); // may be random
 		this.id = id;
+
+		this.pressRight = false;
+		this.pressLeft = false;
+		this.pressUp = false;
+		this.pressDown =false;
 	}
 
 	update() {
 		// update Player
 		// ex) this.x = this.x + 3;
+		this.move();
+	}
+
+	move() {
+		console.log(this.pressRight);
+	    if (this.pressRight) {
+	      	this.x += 3;
+	    } else if (this.pressLeft) {
+	      	this.x -= 3;
+	    } else {
+
+	    }
 	}
 
 	static updateList(list) {
@@ -24,6 +41,7 @@ class Player extends Ship{
 				y: list[i].y,
 			});
 		}
+		//console.log(pack);
 
 		return pack;
     }
