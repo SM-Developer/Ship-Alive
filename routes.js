aee03d619d0cc85803b35f6eb78d5c606d491e84
clucle <wjdenwls123@gmail.com>
2017-07-20 03:31:02 +0900

    feat: lobby basic frame

--- routes.js
+++ routes.js
@@ -1,15 +1,15 @@
 module.exports = function(app){
 
 	app.get('/', function(req, res) {
-	  res.sendFile(__dirname + '/client/game.html');
+	  res.sendFile(__dirname + '/client/public/game.html');
 	});
 
 	app.get('/login', function(req, res) {
-	  res.sendFile(__dirname + '/client/login.html');
+	  res.sendFile(__dirname + '/client/public/login.html');
 	});
 
 	app.get('/lobby', function(req, res) {
-	  res.sendFile(__dirname + '/client/lobby.html');
+	  res.sendFile(__dirname + '/client/public/lobby.html');
 	});
 
 }
